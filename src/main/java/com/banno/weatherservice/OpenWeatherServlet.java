2381a28083f004219523627db580d133dd90ee2c
Martin Naskovski <martin@naskovski.info>
2022-03-28 14:21:34 -0700

    add conditions, C symbol, formatting

--- src/main/java/com/banno/weatherservice/OpenWeatherServlet.java
+++ src/main/java/com/banno/weatherservice/OpenWeatherServlet.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.Validate;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class OpenWeatherServlet extends HttpServlet
 {
@@ -21,17 +21,17 @@ public class OpenWeatherServlet extends HttpServlet
     private String tempLabel(double temp)
     {
         if (temp < 0) {
-            return "Freezing";
-        } else if (temp < 5) {
-            return "Cold";
-        } else if (temp < 10) {
-            return "Chilly";
-        } else if (temp < 15) {
-            return "Cool";
-        } else if (temp < 28) {
-            return "Warm";
+            return "freezing";
+        } else if (temp < 5.0) {
+            return "cold";
+        } else if (temp < 10.0) {
+            return "chilly";
+        } else if (temp < 15.0) {
+            return "cool";
+        } else if (temp < 28.0) {
+            return "warm";
         } else {
-            return "Hot";
+            return "hot";
         }
     }
 
@@ -49,14 +49,28 @@ public class OpenWeatherServlet extends HttpServlet
             JsonNode currWeather = weatherCli.oneCall(lat, lon);
             double currTemp = currWeather.get("current").get("temp").asDouble();
 
-            response.getWriter().println("<h3>Temp: "
-                    + currTemp + " C (Feels: " + tempLabel(currTemp) + ")</h3>");
+            response.getWriter().println("Current conditions: <p>"
+                    + currTemp + " <span>&#8451;</span> (feels " + tempLabel(currTemp) + "), ");
+
+            if (currWeather.get("current").get("weather").isArray()) {
+                Iterator<JsonNode> iter = currWeather.get("current").get("weather").iterator();
+                while (iter.hasNext()) {
+                    final JsonNode objNode = iter.next();
+                    String desc = objNode.get("description").asText();
+                    response.getWriter().print(desc);
+                    if (iter.hasNext()) {
+                        response.getWriter().print(",");
+                    }
+                }
+            }
+
+            response.getWriter().println("</p>Alerts:");
 
             if (currWeather.get("alerts").isArray()) {
                 for (final JsonNode objNode : currWeather.get("alerts")) {
                     String desc = objNode.get("description").asText();
                     desc = desc.replaceAll("\\n", "<br>");
-                    response.getWriter().println("<h3>Alert: <pre>" + desc+ "</pre>");
+                    response.getWriter().println("<br><pre>" + desc+ "</pre></br>");
                 }
             }
 
